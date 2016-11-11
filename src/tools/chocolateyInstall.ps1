e21982de0c5b1f01120b3f991eec56aed254f0af
Sumerano, Michael J <msumerano@gmail.com>
2016-11-11 01:08:11 -0500

    Added checksum

--- src/tools/chocolateyInstall.ps1
+++ src/tools/chocolateyInstall.ps1
@@ -1,8 +1,13 @@
-$packageName = 'SqlSearch'
-$installerType = 'EXE'
-$url = 'https://download.red-gate.com/SQL_Search.exe'
-$silentArgs = '/IAgreeToTheEula'
-$validExitCodes = @(0)
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+$ErrorActionPreference = 'Stop'
 
+$packageArgs = @{
+  packageName    = 'SqlSearch'
+  installerType  = 'EXE'
+  url            = 'https://download.red-gate.com/SQL_Search.exe'
+  checksum       = '9166AE7570F204CD91452FF1C609C084E357F3C677BCBF3CF1BEE9E8D2BCA256'
+  checksumType   = 'sha256'
+  silentArgs     = '/IAgreeToTheEula'
+  validExitCodes = @(0)
+}
 
+Install-ChocolateyPackage @packageArgs
